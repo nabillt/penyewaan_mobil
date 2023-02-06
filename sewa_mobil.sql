375526b6d122894a2cb74807a07f789e160fd05e
nabillt <103652097+nabillt@users.noreply.github.com>
2023-02-06 14:28:56 +0700

    Add files via upload

---
+++ sewa_mobil.sql
@@ -0,0 +1,116 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 06, 2023 at 05:11 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sewa_mobil`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `karyawan`
+--
+
+CREATE TABLE `karyawan` (
+  `id_karayawan` varchar(100) NOT NULL,
+  `nama_karyawan` varchar(300) NOT NULL,
+  `alamat_karyawan` varchar(300) NOT NULL,
+  `kontak` varchar(100) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mobil`
+--
+
+CREATE TABLE `mobil` (
+  `id_mobil` varchar(100) NOT NULL,
+  `nomor_mobil` varchar(100) NOT NULL,
+  `merk` varchar(200) NOT NULL,
+  `jenis` varchar(200) NOT NULL,
+  `warna` varchar(200) NOT NULL,
+  `tahun_pembuatan` varchar(20) NOT NULL,
+  `biaya_sewa_perhari` double NOT NULL,
+  `image` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pelanggan`
+--
+
+CREATE TABLE `pelanggan` (
+  `id_pelanggan` varchar(200) NOT NULL,
+  `nama_pelanggan` varchar(300) NOT NULL,
+  `alamat_pelanggan` varchar(400) NOT NULL,
+  `kontak` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sewa`
+--
+
+CREATE TABLE `sewa` (
+  `id_sewa` varchar(200) NOT NULL,
+  `id_mobil` varchar(100) NOT NULL,
+  `id_karyawan` varchar(100) NOT NULL,
+  `id_pelanggan` varchar(100) NOT NULL,
+  `tgl_sewa` date NOT NULL,
+  `tgl_kembali` date NOT NULL,
+  `total_bayar` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `karyawan`
+--
+ALTER TABLE `karyawan`
+  ADD PRIMARY KEY (`id_karayawan`);
+
+--
+-- Indexes for table `mobil`
+--
+ALTER TABLE `mobil`
+  ADD PRIMARY KEY (`id_mobil`);
+
+--
+-- Indexes for table `pelanggan`
+--
+ALTER TABLE `pelanggan`
+  ADD PRIMARY KEY (`id_pelanggan`);
+
+--
+-- Indexes for table `sewa`
+--
+ALTER TABLE `sewa`
+  ADD PRIMARY KEY (`id_sewa`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
